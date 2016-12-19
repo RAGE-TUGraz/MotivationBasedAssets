8ca8302a7abe49f1c1f2bfabd2a911445f7bf32c
mmaurer <mmaurer@192.168.222.157>
2016-12-19 13:07:10 +0100

    removed default template values for MotivationBasedAdaptationAssetSettings class

--- MotivationBasedAdaptionAsset/MotivationBasedAdaptionAssetSettings.cs
+++ MotivationBasedAdaptionAsset/MotivationBasedAdaptionAssetSettings.cs
@@ -51,71 +51,12 @@ namespace MotivationBasedAdaptionAssetNameSpace
             : base()
         {
             // Set Default values here.
-            TestProperty = "Hello Default World";
-            TestList = new String[] { "Red", "Green", "Blue" };
-            TestPrivate = true;
         }
 
         #endregion Constructors
 
         #region Properties
 
-        /// <summary>
-        /// Gets or sets the test property.
-        /// </summary>
-        ///
-        /// <value>
-        /// The test property.
-        /// </value>
-        [XmlElement()]
-        public String TestProperty
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// Gets the string[].
-        /// </summary>
-        ///
-        /// <value>
-        /// .
-        /// </value>
-        [XmlArray()]
-        [XmlArrayItem("ListItem")]
-        public String[] TestList
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// Gets a value indicating whether the test read only.
-        /// </summary>
-        ///
-        /// <value>
-        /// true if test read only, false if not.
-        /// </value>
-        public Boolean TestReadOnly
-        {
-            get
-            {
-                return true;
-            }
-        }
-
-        /// <summary>
-        /// Gets a value indicating whether the test private.
-        /// </summary>
-        ///
-        /// <value>
-        /// true if test private only, false if not.
-        /// </value>
-        public Boolean TestPrivate
-        {
-            get;
-            private set;
-        }
 
         #endregion Properties
     }
